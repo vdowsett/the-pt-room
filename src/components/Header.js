4c2ea54ad4251d489f294564032f78f55d9cb8da
vdowsett <vdowsett@gmail.com>
2019-04-02 10:35:54 -0500

    justify nav end

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,27 +4,28 @@ import { Link } from 'react-router-dom';
 
 const Header = () => (
   <header className="pt-3 pb-3 bg-light">
-    <div class="container">
+    <div className="container">
+        
+        <div className="row navbar navbar-expand-lg navbar-light ">
+          <div className="col-lg-4">
+            <a className="navbar-brand" href="/"> <img src={logo} alt="logo" className="w-100"/> </a> 
+          </div>
+          <div className="col-lg-8">
+            <button className="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
 
-      <nav className="navbar navbar-expand-lg navbar-light">
-
-        <a className="navbar-brand" href="/"> <img src={logo} alt="logo" className="w-100"/> </a> 
-
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <Link to='/' className="nav-link">Home</Link>
-          <Link to='/meet-jenny' className="nav-link">Meet Jenny</Link>
-          <Link to='/services' className="nav-link">Services</Link>
-          <Link to='/new-clients' className="nav-link">New Clients</Link>
-          <Link to='/bookings' className="nav-link">Bookings</Link>
-          <Link to='/contact' className="nav-link">Contact</Link>                
+            <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+              <Link to='/' className="nav-link">Home</Link>
+              <Link to='/meet-jenny' className="nav-link">Meet Jenny</Link>
+              <Link to='/services' className="nav-link">Services</Link>
+              <Link to='/new-clients' className="nav-link">New Clients</Link>
+              <Link to='/bookings' className="nav-link">Bookings</Link>
+              <Link to='/contact' className="nav-link">Contact</Link>                
+            </div>
+          </div>
         </div>
-
-      </nav>
-    </div>
+      </div>
   </header>
 );
 
