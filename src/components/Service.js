2f48cf8c3fcafcb463b073a70ac519ba665d977c
vdowsett <vdowsett@gmail.com>
2019-04-04 11:43:14 -0500

    adding pre and post events

--- src/components/Service.js
+++ src/components/Service.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import servicesData from './../data/services';
 import ReactHtmlParser from 'react-html-parser';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBullseye, faBurn, faFireAlt, faSpa, faDumbbell, faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faBullseye, faBurn, faFireAlt, faSpa, faDumbbell, faHeart, faSwimmer, faRunning, faFire } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-library.add(faBullseye, faBurn, faFireAlt, faSpa, faDumbbell, faHeart)
+library.add(faBullseye, faBurn, faFireAlt, faSpa, faDumbbell, faHeart, faSwimmer, faRunning, faFire)
 
 class Service extends Component {
 
@@ -26,11 +26,11 @@ class Service extends Component {
               <p className="subtitle m-0">Get back to your best you</p>
               <h2>I'm proud to offer the following services</h2>
 
-        <div className="row p-5 mb05">
+        <div className="row p-5">
       { 
         this.state.services.map( (service, index) => 
 
-          <div key={index} className="col-md-4">
+          <div key={index} className="col-md-4 mb-5">
 
             <div className="services-icon d-flex justify-content-center align-items-center">
               <FontAwesomeIcon className="p-1" icon={service.icon} />
