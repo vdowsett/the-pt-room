61a847fc7238b5b4ee9b7c68bff0a1f0c5a8e7a2
vdowsett <vdowsett@gmail.com>
2019-04-15 11:48:34 -0500

    still routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 import Home from './components/Home';
 import MeetJenny from './components/MeetJenny';
@@ -8,28 +11,32 @@ import NewClients from './components/NewClients';
 import Services from './components/Services';
 import Bookings from './components/Bookings';
 import Contact from './components/Contact';
-import Header from './components/Header';
-import Footer from './components/Footer';
 import Service from './components/Service';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
-      <HashRouter><Switch>
-      <div className="App">
-        <Header></Header>
-        <main>
-            <Route exact path="/" component={Home} />
-            <Route path="/meet-jenny" component={MeetJenny} />
-            <Route path="/new-clients" component={NewClients} />
-            <Route path="/services" component={Services} />
-            <Route path="/services/:slug" component={Service} />
-            <Route path="/bookings" component={Bookings} />
-            <Route path="/contact" component={Contact} />
-        </main>
-        <Footer></Footer>
-      </div>
-      </Switch></HashRouter>
+      <Router basename="spna.dowsettdesigns.com/" >
+        <div className="App">
+          <Header></Header>
+          <main>
+            <Switch>
+              <Route exact path="/" component={Home} />
+                <Route path="/meet-jenny" component={MeetJenny} />
+                <Route path="/new-clients" component={NewClients} />
+                <Route path="/services" component={Services} />
+                <Route path="/services/:slug" component={Service} />
+                <Route path="/bookings" component={Bookings} />
+                <Route path="/contact" component={Contact} />
+            </Switch>
+          </main>
+          <Footer></Footer>
+        </div>
+      </Router>
     );
   }
 }
