b2a6c17de704a7a0bb0af9172e05b9d9be1d7d48
vdowsett <vdowsett@gmail.com>
2019-04-15 13:41:56 -0500

    back to hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 
 import Header from './components/Header';
