63cda7d61f035b2a20387f87c5fab12aa3fbe457
vdowsett <vdowsett@gmail.com>
2020-12-22 14:03:02 -0600

    adding a just in case backup file

---
+++ backup/ThePTRoom-backup-12-22-20/precache-manifest.e3e5497376c0d09d81f15e9441e2176d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "d88393ac1562b9808b086e516a489b7f",
+    "url": "/static/media/Jenny-70.3-2014.d88393ac.jpg"
+  },
+  {
+    "revision": "bfcc8f93aff192f13504",
+    "url": "/static/css/main.8da7509f.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5c578d8aab4b6771c806",
+    "url": "/static/js/2.c4481968.chunk.js"
+  },
+  {
+    "revision": "909b9cd7ff197bc26c81cfdaeed5c02e",
+    "url": "/static/media/logo.909b9cd7.svg"
+  },
+  {
+    "revision": "dcb9e48b6cbfa8c496d865745bcf3b9a",
+    "url": "/static/media/jenny-strecker-personal-trainer.dcb9e48b.jpg"
+  },
+  {
+    "revision": "de5d4c51e5020756b4435e23a9d24136",
+    "url": "/static/media/Ironman-Auckland-2014-Jenny.de5d4c51.png"
+  },
+  {
+    "revision": "fe998faaad3029f99d30571a280a4206",
+    "url": "/static/media/Jenny-Beach-Series.fe998faa.png"
+  },
+  {
+    "revision": "bfcc8f93aff192f13504",
+    "url": "/static/js/main.afb1c557.chunk.js"
+  },
+  {
+    "revision": "4179898af5e12147bf83fc0355e432d0",
+    "url": "/static/media/MAX-Logo1.4179898a.png"
+  },
+  {
+    "revision": "4decf22467754082885dcc8040fc0b5b",
+    "url": "/static/media/REPstrainer3.4decf224.jpg"
+  },
+  {
+    "revision": "d03457eb032673dadbfad89e30f3ded7",
+    "url": "/static/media/linen-dark.d03457eb.jpg"
+  },
+  {
+    "revision": "0662f318351bd9ae7de708d19a88f6db",
+    "url": "/static/media/winter.0662f318.jpg"
+  },
+  {
+    "revision": "361e08dcb361415dcf3ceb4b10c44dbc",
+    "url": "/static/media/hot-rocks.361e08dc.jpg"
+  },
+  {
+    "revision": "ccdfacfae485e0ded7a1f52ea733e491",
+    "url": "/static/media/ironman-cowbell.ccdfacfa.jpg"
+  },
+  {
+    "revision": "c039b2e5df3552c94cc4067ee2a0a4db",
+    "url": "/index.html"
+  }
+];
